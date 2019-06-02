462db7e93845cef10651fb4eda270d42b54927db
Ibrahim  Akar <ibsakar@gmail.com>
2019-06-02 18:37:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,8 @@
 # Your code goes here!
 class Anagram 
-  attr_accessor :name 
+  attr_accessor :word 
 
- def initialize(name)
-   @name = name 
+ def initialize(word)
+   @word = word 
   end
 end
